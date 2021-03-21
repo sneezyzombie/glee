babc6197c0a248b005ebcc15e68cc8438b266871
sneezyzombie <dan21a@yandex.ru>
2021-03-22 00:13:03 +0300

    productsoftheweek

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 $(function(){
-    $(".top-slider__wrapper").slick({
+    $('.top-slider__wrapper').slick({
         arrows:false,
         dots:true,
         slidesToShow:1,
         slidesToScroll:1,
         fade:true,
-    })
+    });
+    var mixer = mixitup('.filters__btn');
+    var mixer = mixitup('.products-top__inner')
 });
