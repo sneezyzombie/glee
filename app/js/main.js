d19950b18e975a94f75b6b48b65dd51f0c99b980
sneezyzombie <dan21a@yandex.ru>
2021-03-25 20:07:38 +0300

    productlists

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
 $(function(){
+
+    $(".shop__filter__price__range-slider").ionRangeSlider({
+        type: "double",
+        step:"0.01",
+        onChange: function (data) {
+           $(".shop__filter__price__number__from").text(data.from);
+           $(".shop__filter__price__number__to").text(data.to);
+        },
+    });
+
+    $(".star-rate").rateYo({
+        starWidth: "10px",
+        starHeight:"10px",
+        ratedFill: "#ffcc00",
+        readOnly: true
+      });
+      $(".shop-content__page-2__item__star-rate").rateYo({
+        starWidth: "20px",
+        starHeight:"20px",
+        ratedFill: "#ffcc00",
+        readOnly: true
+      });
+
     $('.top-slider__wrapper').slick({
         arrows:false,
         dots:true,
@@ -9,4 +32,5 @@ $(function(){
     var mixer = mixitup('.filters__btn');
     var mixer = mixitup('.products-top__inner');
     var mixer = mixitup('.design__inner');
+    var mixer = mixitup('.filters__btn');
 });
